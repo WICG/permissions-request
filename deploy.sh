c9d07d6a51f2d1ec1c57d3f578cdc872280a6b4d
Jeffrey Yasskin <jyasskin@google.com>
2017-09-27 16:47:33 -0700

    Install Bikeshed, and use a deploy script to send output to gh-pages.

--- deploy.sh
+++ deploy.sh
@@ -1,59 +1,43 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
-SOURCE_BRANCH="master"
-TARGET_BRANCH="gh-pages"
+# Expects that the build script will place the contents of gh-pages into out/.
+# Expects that the before_deploy steps will place a deploy key in ./deploy_key.
 
-function doCompile {
-  # --fail makes bad bikeshed files fail the build.
-    if ! curl --fail https://api.csswg.org/bikeshed/ -F file=@index.bs -o out/index.html; then
-        curl https://api.csswg.org/bikeshed/ -F file=@index.bs -F output=err|sed 's/\\033/'$(printf "\e")/g
-        exit 1
-    fi
-}
-
-# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-    echo "Skipping deploy; just doing a build."
-    mkdir out
-    doCompile
-    exit 0
-fi
+TARGET_BRANCH=gh-pages
 
 # Save some useful information
 REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 
+CLONED_TARGET_BRANCH=$(mktemp -d)
 # Clone the existing gh-pages for this repo into out/
-git clone --depth 5 --branch $TARGET_BRANCH $REPO out
+git clone --depth 1 --branch $TARGET_BRANCH $REPO $CLONED_TARGET_BRANCH
 
-# Clean out existing contents
-rm -rf out/* || exit 0
+# Copy over the out/ directory, removing any old contents.
+rsync -r --exclude .git --delete out $CLONED_TARGET_BRANCH
 
-# Run our compile script
-doCompile
+# Install the deploy key.
+chmod 600 deploy_key
+eval `ssh-agent -s`
+ssh-add deploy_key
 
 # Now let's go have some fun with the cloned repo
-cd out
-git config user.name "Travis CI"
-git config user.email "$COMMIT_AUTHOR_EMAIL"
-
+cd $CLONED_TARGET_BRANCH
 # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
 if git diff --quiet; then
     echo "No changes to the output on this push; exiting."
     exit 0
 fi
 
+git config user.name "Travis CI"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
+
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
 git add -A .
 git commit -m "Deploy to GitHub Pages: ${SHA}"
-
-# Install the deploy key.
-chmod 600 ../deploy_key
-eval `ssh-agent -s`
-ssh-add ../deploy_key
 
 # Now that we're all set up, we can push.
 git push $SSH_REPO $TARGET_BRANCH
